46a4671bc462d8ad69ffe748a2e30b800e75fdf4
AlejooGonzalez <gonzalezalejo918@gmail.com>
2026-05-06 15:38:37 -0300

    Reestructuracion

---
+++ .next/dev/static/chunks/src_app_layout_tsx_004glpo._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/[root-of-the-server]__02nfq85._.css","static/chunks/node_modules_@clerk_nextjs_dist_esm_app-router_09a1hcw._.js","static/chunks/node_modules_04n~3~e._.js"],
+    source: "dynamic"
+});
